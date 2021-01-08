0342bab19c3baf0544d8fb22d2a615ca6e1ce4d6
Sally Golding <lady_snowblood@hotmail.com>
2021-01-08 15:13:45 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello")
+user_starter = input("What is your favorite starter?")
+user_main = input("What is your favourite main course?")
+user_dessert = input("What is your favourite dessert?")
+user_drink = input("What is your favourite drink?")
+print("Your favourite meal is ", user_starter, user_main, user_dessert, "with a glass of" ,user_drink)
+
+
+
+
+
+
+
+
+
+
+
